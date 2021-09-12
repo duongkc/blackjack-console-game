050f8d2d35b7c99db78106661f465bd4e6505a69
KC <kimchauduong@gmail.com>
2021-09-12 15:46:50 +0200

    Create main execution file for game

---
+++ src/main/java/blackjack/Blackjack.java
@@ -0,0 +1,14 @@
+package blackjack;
+
+/**
+ * A classic game of blackjack. played solely through console commands.
+ *
+ * @author Kim Chau Duong
+ * @version 1.0
+ */
+public class Blackjack {
+
+    public static void main(String[] args) {
+
+    }
+}
