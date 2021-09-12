cb5859e2a4e1869698b3d717f4036f68874b4418
KC <kimchauduong@gmail.com>
2021-09-12 16:32:00 +0200

    Create Deck class
    For generating a deck consisting of cards.
    still unfinished.

---
+++ src/main/java/blackjack/Deck.java
@@ -0,0 +1,16 @@
+package blackjack;
+
+import blackjack.model.Card;
+
+/**
+ * Class to
+ *
+ * @author Kim Chau Duong
+ * @version 1.0
+ */
+public class Deck {
+    String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+
+}
