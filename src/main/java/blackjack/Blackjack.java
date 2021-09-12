04d2dc2e1ef258d673d0cbb45dc100cf156ee30f
KC <kimchauduong@gmail.com>
2021-09-12 20:46:24 +0200

    Remove redundant code

--- src/main/java/blackjack/Blackjack.java
+++ src/main/java/blackjack/Blackjack.java
@@ -15,8 +15,7 @@ public class Blackjack {
 
     public static void main(String[] args) {
         Deck deck = new Deck();
-//        deck.createDeck();
-//        deck.shuffleDeck();
+
         System.out.println(Arrays.toString(deck.currentDeck.toArray()));
     }
 }
