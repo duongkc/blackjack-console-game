0644a5144fbc42601a30f30e4a4f7964eebd041b
KC <kimchauduong@gmail.com>
2021-09-12 17:07:08 +0200

    Change deck list to stack
    This change makes it easier to deal cards during the game.

--- src/main/java/blackjack/Blackjack.java
+++ src/main/java/blackjack/Blackjack.java
@@ -3,6 +3,7 @@ package blackjack;
 import blackjack.model.Card;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * A classic game of blackjack. played solely through console commands.
@@ -14,7 +15,7 @@ public class Blackjack {
 
     public static void main(String[] args) {
         Deck deck = new Deck();
-        Card[] newDeck = deck.createDeck();
-        System.out.println(Arrays.toString(newDeck));
+        Stack<Card> newDeck = deck.createDeck();
+        System.out.println(Arrays.toString(newDeck.toArray()));
     }
 }
--- src/main/java/blackjack/Deck.java
+++ src/main/java/blackjack/Deck.java
@@ -2,7 +2,7 @@ package blackjack;
 
 import blackjack.model.Card;
 
-import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * Class to
@@ -14,13 +14,14 @@ public class Deck {
     String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
     String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-    Card[] createDeck() {
-        Card[] deck = new Card[52];
+    Stack<Card> createDeck() {
+        Stack<Card> deck = new Stack<>();
         int deckIndex = 0;
         for (String suit : suits) {
             for (String value : values) {
-                deck[deckIndex] = new Card(suit, value);
-                deckIndex++;
+//                deck[deckIndex] = new Card(suit, value);
+//                deckIndex++;
+                deck.push(new Card(suit, value));
             }
 
         }
