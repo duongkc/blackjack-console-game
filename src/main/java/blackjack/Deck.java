07f784745dbbed43908ef61f9518a92e01b56f94
KC <kimchauduong@gmail.com>
2021-09-12 20:20:15 +0200

    Provide documentation to Deck class

--- src/main/java/blackjack/Deck.java
+++ src/main/java/blackjack/Deck.java
@@ -15,24 +15,29 @@ public class Deck {
     String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
     String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     Stack<Card> currentDeck = new Stack<>();
+    Card topCard;
 
     public Deck() {
         createDeck();
         shuffleDeck();
     }
 
+    /**
+     * Creates new deck of cards
+     */
     void createDeck() {
         int deckIndex = 0;
         for (String suit : suits) {
             for (String value : values) {
-//                deck[deckIndex] = new Card(suit, value);
-//                deckIndex++;
+                // Puts new card on top of the stack
                 this.currentDeck.push(new Card(suit, value));
             }
         }
     }
 
-
+    /**
+     * Shuffles deck of cards
+     */
     void shuffleDeck() {
         Collections.shuffle(this.currentDeck);
     }
