bdda9b361f7b6e6bb8661659fa59070b669ece36
KC <kimchauduong@gmail.com>
2021-09-12 16:05:49 +0200

    Add Card model class

---
+++ src/main/java/blackjack/model/Card.java
@@ -0,0 +1,29 @@
+package blackjack.model;
+
+/**
+ * Card Model required to build a card deck with playing cards
+ *
+ * @author Kim Chau Duong
+ * @version 1.0
+ */
+public class Card {
+    private String suit;
+    private String value;
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getPlayingCard() {
+        return value + " of " + suit;
+    }
+
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
